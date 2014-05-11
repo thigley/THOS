d5f8d14b2b0bb7147943799f9233c47126b37b52
Default User <oem@freekbox.fglan>
2014-05-10 18:23:43 -0700

    Hello OS

---
+++ #1_HelloOS/boot.asm
@@ -0,0 +1,45 @@
+;****************************************
+;	boot1.asm
+;	- Basic Bootloader
+;	- Created by Tyler Higley
+;	- brokenthorn.com Tutorial
+;****************************************
+
+org	0x7c00				; The BIOS loads at location 0x7c00.
+					; This instruction insures all addresses will be relative to 0x7c00
+
+bits	16				; Must start in 16 bit mode
+
+Start:
+	mov bx, LoadMSG
+	call printString
+
+	jmp $
+
+	cli				; Clears interupts
+	hlt				; Halts the system
+
+LoadMSG:
+	db 'Hello Operating System', 0
+
+printString:
+	pusha
+	mov ah, 0x0e
+
+	loop:
+	mov al, [bx]
+	cmp al, 0
+	je end
+
+	int 0x10
+	inc bx
+	jmp loop
+	end:
+
+	popa
+	ret
+
+times 510 - ($-$$) db 0			; Pad extra space with zeros
+					; $ - current address, $$ - address of first instruction
+
+dw 0xAA55				; Boot Signature
