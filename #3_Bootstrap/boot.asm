75beba138b5f563bb51adaa80fa9efa0a33bf0a2
Tyler Higley <tylerjhigley@gmail.com>
2014-06-22 23:18:36 -0700

    commented out broken stuff

--- #3_Bootstrap/boot.asm
+++ #3_Bootstrap/boot.asm
@@ -12,9 +12,9 @@
 
 
 [org 0x7c00]
-KERNEL_OFFSET equ 0x1000	;memoy offset of kernel
+;KERNEL_OFFSET equ 0x1000	;memoy offset of kernel
 
-mov [BOOT_DRIVE], dl		;store boot drive for later
+;mov [BOOT_DRIVE], dl		;store boot drive for later
 
 mov bp, 0x9000 ;set the stack
 mov sp, bp
@@ -26,7 +26,7 @@ call clearscreen
 mov si, MSG_REAL
 call println
 
-call load_kernel
+;call load_kernel
 
 ;switch to protected mode
 call switch_to_pm
@@ -35,22 +35,22 @@ jmp $
 
 ;INCLUDE FILES
 %include 'methods/printMethods.asm'
-%include 'methods/disk_load.asm'
+;%include 'methods/disk_load.asm'
 %include 'gdt.asm'
 %include 'methods/pmPrint.asm'
 %include 'switch.asm'
 
-[bits 16]
-load_kernel:
-	mov bx, MSG_KERN
-	call println
-
-	mov bx, KERNEL_OFFSET
-	mov dh, 15
-	mov dl, [BOOT_DRIVE]
-	call disk_load
-	
-	ret
+;[bits 16]
+;load_kernel:
+;	mov bx, MSG_KERN
+;	call println
+;
+;	mov bx, KERNEL_OFFSET
+;	mov dh, 15
+;	mov dl, [BOOT_DRIVE]
+;	call disk_load
+;	
+;	ret
 
 [bits 32]
 BEGIN_PM:
@@ -58,14 +58,14 @@ BEGIN_PM:
 	mov ebx, MSG_PROT
 	call print_string_pm
 
-	call KERNEL_OFFSET
+;	call KERNEL_OFFSET
 	jmp $
 
 ;DATA
 BOOT_DRIVE	db 0
 MSG_REAL	db "Started in 16-bit Real Mode", 0
 MSG_PROT	db "Successfully landed in 32-bit Protected Mode", 0
-MSG_KERN	db "Loadint kernel into memory...", 0
+;MSG_KERN	db "Loadint kernel into memory...", 0
 
 times 510 - ($-$$) db 0
 dw 0xAA55
