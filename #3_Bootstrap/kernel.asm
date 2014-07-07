d0570b3b6093ef5cb266a80a7dcf5459eb3751e0
Tyler Higley <tylerjhigley@gmail.com>
2014-07-07 00:42:13 -0700

    Jump and 32

--- #3_Bootstrap/kernel.asm
+++ #3_Bootstrap/kernel.asm
@@ -8,9 +8,21 @@ main:
 	mov si, msg
 	call println
 	
-;	mov ebx, msg
-;	mov ax, 4
-;	call print_string_pm
-
+	call switch_to_pm
 loops:
 	jmp loops
+
+%include 'gdt.asm'
+%include 'methods/pmPrint.asm'
+%include 'switch.asm'
+
+MSG_PROT        db "Successfully landed in 32-bit Protected Mode", 0
+
+[bits 32]
+BEGIN_PM:
+
+      	mov ebx, MSG_PROT
+       	mov ax, 5
+       	call print_string_pm
+	jmp $
+
