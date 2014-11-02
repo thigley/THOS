536949787b47306fb80bfd81a21b23b2c5e528c3
thigley <tylerjhigley@gmail.com>
2014-11-02 09:45:13 -0800

    getLine

--- Latest_Version/src/kernel.c
+++ Latest_Version/src/kernel.c
@@ -3,7 +3,60 @@
 #include "C_files/builtin.c"
 #include "C_files/keyboard.c"
 
+void loadWelcome();
+void typePrompt();
+void readCommand();
+char* nextLine();
+char inputLine[50] = {0};
 void main() {
+	setup();
+	loadWelcome();
+	while(TRUE){
+		typePrompt();
+		readCommand();
+//		executeCommand();
+	}
+}
+
+void loadWelcome(){
+	println("Welcome to THOS!");
+}
+
+void typePrompt(){
+	print("# ");
+}
+
+void readCommand(){
+	char* input = nextLine();
+	print("\n");
+	print("---Your input was ");
+	print(input);
+	println("---");
+	//int command = getCommand(input);
+	//char* param = getParam(input);
+}
+
+char* nextLine(){
+	int start = OFFSET;
+	while(TRUE){
+		if(inportb(0x60)==250) continue;
+		if(inportb(0x60)&0x80) continue;
+		char next = getchar();
+		if(next=='\n') break;
+		else printchar(next);
+	}
+	int last = OFFSET;
+	int length = (last-start)/2;
+	
+	int i;
+	for(i=0; i<length; i++){
+		inputLine[i]= vidmem[start+i*2];
+	}
+	inputLine[length] = 0;
+	return inputLine;
+	
+}
+/*void main() {
 	CURRENTBACK = LIGHTBLUE;
 	setup();
 	loadHello();
@@ -27,4 +80,4 @@ void main() {
 	}
 	for(;;);
 }
-
+*/
